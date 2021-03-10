c2e98473ccb92e2597e3aa049ed2ca679f1fd63e
sun <sun@DESKTOP-GP6QFAV>
2021-03-10 17:06:00 +0000

    10/03

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -2,14 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal : IEntityRepository<Product>
+    public interface ICategoryDal:IEntityRepository<Category>
     {
-        List<Product> GetAll(Expression<Func<Product, bool>> filter = null);
+        
     }
 }
