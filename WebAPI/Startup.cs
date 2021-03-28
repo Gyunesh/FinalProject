47eef496c0d54ca761432cfabf21064d496acded
sun <sun@DESKTOP-GP6QFAV>
2021-03-28 21:11:58 +0100

    28/03

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -47,6 +47,8 @@ namespace WebAPI
            
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -86,6 +88,8 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
