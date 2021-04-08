3c68bc265a74098eb6ed895a83f73ec1e70e1ad8
sun <sun@DESKTOP-GP6QFAV>
2021-04-08 22:29:17 +0100

    08/04

--- Core/Extensions/ErrorDetails.cs
+++ Core/Extensions/ErrorDetails.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation.Results;
 using Newtonsoft.Json;
 
 namespace Core.Extensions
@@ -11,10 +12,16 @@ namespace Core.Extensions
     {
         public string Message { get; set; }
         public int StatusCode { get; set; }
+       
 
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
         }
+    }
+
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> Errors { get; set; }
     }
 }
