fd88a36b4333168070f5f939e837b1bafe11b673
sun <sun@DESKTOP-GP6QFAV>
2021-04-08 22:11:00 +0100

    08/04

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -89,7 +89,9 @@ namespace WebAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
-            app.UseCors(builder=>builder.WithOrigins("http://localhost:4400").AllowAnyHeader().AllowAnyOrigin());
+            app.ConfigureCustomExceptionMiddleware();
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyOrigin());
 
             app.UseHttpsRedirection();
 
